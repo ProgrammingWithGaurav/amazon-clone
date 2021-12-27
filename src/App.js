e6963f47998906dec294706348e678741bd7fbdf
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-12-27 13:13:07 +0530

    Adding Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import Header from './components/Header/Header';
 
 function App() {
     return (
         <div className='app'>
-          hello
+            <Header />
         </div>
     )
 }
