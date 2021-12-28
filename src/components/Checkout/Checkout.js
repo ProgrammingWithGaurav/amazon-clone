b691509de2e7ee8f92afb84d8a37e72103fc6ab3
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-12-28 10:33:01 +0530

    Adding User SignIn And SignOUt

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from '../../StateProvider';
 import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
 
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
     return (
         <div className='checkout'>
             <div className="checkout__left">
@@ -16,9 +16,10 @@ function Checkout() {
                 />
 
                 <div>
+                    <h3>Hello, {user.email}</h3>
                     <h2 className="checkout__title">Your shopping Basket</h2>
                     {basket.map(item => (
-                        <CheckoutProduct 
+                        <CheckoutProduct
                             id={item.id}
                             title={item.title}
                             image={item.image}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,16 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from '../../StateProvider';
+import { auth } from "firebase";
 
 function Header() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
+
+    const handleAuthentication = () => {
+        if(user) {
+            auth().signOut();
+        }
+    }
     return (
         <div className="header">
             <Link to='/'>
@@ -23,10 +30,10 @@ function Header() {
             </div>
 
             <div className="header__nav">
-                <Link to='/login'>
-                    <div className="header__option">
+                <Link to={!user && '/login'}>
+                    <div className="header__option" onClick={handleAuthentication}>
                         <span className="header__optionLineOne">Hello Guests</span>
-                        <span className="header__optionLineTwo">Sign In</span>
+                        <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
                     </div>
                 </Link>
 
